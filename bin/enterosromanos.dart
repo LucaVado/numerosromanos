72b26f051565296918ffd392e5175cbd3bffb10e
LucaVado <Luca_Vado@hotmail.com>
2023-02-28 14:57:03 -0700

    TODO EL PROYECTO

---
+++ bin/enterosromanos.dart
@@ -0,0 +1,5 @@
+import 'package:enterosromanos/enterosromanos.dart' as enterosromanos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${enterosromanos.calculate()}!');
+}
---
+++ test/enterosromanos_test.dart
@@ -0,0 +1,8 @@
+import 'package:enterosromanos/enterosromanos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
